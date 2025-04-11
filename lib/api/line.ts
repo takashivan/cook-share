4d4ec8c3a2cc8dbe3e030a816f7eae72ef478584
takashivan <takashi.van822@gmail.com>
2025-04-11 12:20:38 +0900

    変な修正

--- lib/api/line.ts
+++ lib/api/line.ts
@@ -10,10 +10,15 @@ export const CheckLineUser = async (line_user_id: string) => {
 export const LinkLineId = async (
   line_user_id: string,
   name: string,
-  picture: string
+  picture: string,
+  token: string
 ) => {
   const res = await fetch(`${baseURL}/link-line-id`, {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
     body: JSON.stringify({ line_user_id, name, picture }),
   });
   return res.json();
