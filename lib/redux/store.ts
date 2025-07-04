33f8c1aea5e0824c421e8fec548f25d08d969799
A-Kako0430 <araki@scala-com.jp>
2025-07-04 18:21:50 +0900

    delete: 不要なファイル削除

--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -1,14 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import jobsReducer from "./slices/jobsSlice";
 import restaurantsReducer from "./slices/restaurantsSlice";
-import companyJobsReducer from "./slices/companyJobsSlice";
+// import companyJobsReducer from "./slices/companyJobsSlice";
 import operatorReducer from "./slices/operatorSlice";
 
 export const store = configureStore({
   reducer: {
     jobs: jobsReducer,
     restaurants: restaurantsReducer,
-    companyJobs: companyJobsReducer,
+    // companyJobs: companyJobsReducer,
     operator: operatorReducer,
   },
 });
