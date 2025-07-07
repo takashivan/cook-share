4081a332a15dea0515fee06432cf12a709e1f43b
A-Kako0430 <araki@scala-com.jp>
2025-07-07 16:27:59 +0900

    update: 本番でのconsoleの出力を削除する

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
       "xcti-onox-8bdw.n7e.xano.io",
     ],
   },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
 };
 
 module.exports = nextConfig;
