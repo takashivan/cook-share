87130f7ea2cec322354bc18c59c854b4dda22ccc
A-Kako0430 <araki@scala-com.jp>
2025-07-09 10:34:49 +0900

    update: generate API

--- api/__generated__/operator/Companies.ts
+++ api/__generated__/operator/Companies.ts
@@ -10,12 +10,38 @@
  * ---------------------------------------------------------------
  */
 
-import { CompaniesListData } from "./data-contracts";
+import { CompaniesDetailData, CompaniesListData } from "./data-contracts";
 import { HttpClient, RequestParams } from "./http-client";
 
 export class Companies<
   SecurityDataType = unknown,
 > extends HttpClient<SecurityDataType> {
+  /**
+   * @description <br /><br /> <b>Authentication:</b> not required
+   *
+   * @tags companies
+   * @name CompaniesDetail
+   * @request GET:/companies/{company_id}
+   */
+  companiesDetail = (companyId: string, params: RequestParams = {}) =>
+    this.request<CompaniesDetailData, void>({
+      path: `/companies/${companyId}`,
+      method: "GET",
+      format: "json",
+      ...params,
+    });
+
+  companiesDetailQueryArgs = (
+    companyId: string,
+    params: RequestParams = {},
+    enabled: boolean = true,
+  ) => {
+    const key = enabled ? [`/companies/${companyId}`] : null;
+    const fetcher = () =>
+      this.companiesDetail(companyId, params).then((res) => res.data);
+    return [key, fetcher] as const;
+  };
+
   /**
    * @description [AUTHED-Operator]運営者だけが見られる、企業情報一覧 <br /><br /> <b>Authentication:</b> required
    *
