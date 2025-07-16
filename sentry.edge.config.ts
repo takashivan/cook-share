ed63be9ff35d445c7ae88f900c8d9403c3d786bf
A-Kako0430 <araki@scala-com.jp>
2025-07-16 10:11:11 +0900

    update: sentryの環境指定

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -8,6 +8,8 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
+  environment: process.env.NEXT_PUBLIC_VERCEL_ENV ?? 'development',
+
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 0.5,
 
